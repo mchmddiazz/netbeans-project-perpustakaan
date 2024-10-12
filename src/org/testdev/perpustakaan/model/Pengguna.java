a6af90cf2871bf39ec25adda3bbc45de4478c710
mchmddiazz <pomcat.14@gmail.com>
2024-10-12 15:07:42 +0700

    init netbeans project

---
+++ src/org/testdev/perpustakaan/model/Pengguna.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.testdev.perpustakaan.model;
+
+/**
+ *
+ * @author PCL-B00-00
+ */
+public class Pengguna {
+    private String nama;
+    private String id;
+    private long email;
+
+    public Pengguna(String nama, String id, long email) {
+        this.nama = nama;
+        this.id = id;
+        this.email = email;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public long getEmail() {
+        return email;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setEmail(long email) {
+        this.email = email;
+    }
+    
+    
+}
---
+++ src/org/testdev/perpustakaan/model/Pustakawan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.testdev.perpustakaan.model;
+
+/**
+ *
+ * @author PCL-B00-00
+ */
+public class Pustakawan extends Pengguna {
+    
+    public Pustakawan(String nama, String id, long email) {
+        super(nama, id, email);
+    }
+    
+    public boolean tambahBuku(){
+        return true;
+    }
+    
+    public boolean hapusBuku(){
+        return true;
+    }
+    
+    public boolean updateBuku(){
+        return true;
+    }
+    
+}
